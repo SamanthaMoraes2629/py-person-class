14ed63bc7e9cf6b1a8623bebf24d9ad3a1e1a00e
SamanthaMoraes2629 <samanthamoraes.py@outlook.com>
2025-03-04 12:59:49 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,35 +4,36 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None  # Inicializamos com None
+        self.husband = None  # Inicializamos com None
         Person.people[name] = self
 
     def set_spouse(self) -> None:
         if self.wife:
             self.wife = Person.people.get(self.wife)
-        else:
-            None
         if self.husband:
             self.husband = Person.people.get(self.husband)
-        else:
-            None
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-
-    for person in people:
-        person_instance = Person(person["name"], person["age"])
-        person_list.append(person_instance)
+    person_list = [
+        Person(person["name"], person["age"]) for person in people
+    ]
 
     for person in people:
         person_instance = Person.people.get(person["name"])
+        # Atribuir wife ou husband, se existirem
         if "wife" in person and person["wife"]:
             person_instance.wife = Person.people.get(person["wife"])
         else:
-            None
+            # Se não houver "wife", removemos o atributo
+            if hasattr(person_instance, "wife"):
+                del person_instance.wife
         if "husband" in person and person["husband"]:
             person_instance.husband = Person.people.get(person["husband"])
         else:
-            None
+            # Se não houver "husband", removemos o atributo
+            if hasattr(person_instance, "husband"):
+                del person_instance.husband
 
     return person_list
